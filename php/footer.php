f7adf6bf7bb1e0c67e2ecee286b418e80a9bb6d2
Mayank-shanu <mayankjain.shanu@gmail.com>
2017-12-19 21:38:03 +0530

    major part of index done

---
+++ php/footer.php
@@ -0,0 +1,29 @@
+<?php
+
+echo "<footer class=\"footer\">
+  <div class=\"container-fluid\">
+    <div class=\"row text-center\" id=\"footer_content\">
+      <div class=\"col-sm-12 col-lg-4\">
+        <a href=\"index.html\">Home&nbsp;<i class=\"fas fa-home\"></i></a>
+      </div>
+      <div class=\"col-sm-12 col-lg-4\">
+        <a href=\"#\">Sign in/up&nbsp;<i class=\"fas fa-sign-in-alt\"></i></a>
+      </div>
+      <div class=\"col-sm-12 col-lg-4\">
+        <a href=\"#\">Contact Us&nbsp;<i class=\"fab fa-contao\"></i></a>
+      </div>
+    </div>
+    <div class=\"row text-center justify-content-center\" id=\"social_icon\">
+         <div class=\"col-12\">
+           <i class=\"fab fa-facebook fa-2x\" id=\"facebook\"></i>&nbsp;&nbsp;&nbsp;
+           <i class=\"fab fa-twitter fa-2x\" id=\"twitter\"></i>&nbsp;&nbsp;&nbsp;
+           <i class=\"fab fa-github-square fa-2x\" id=\"github\"></i>&nbsp;&nbsp;&nbsp;
+           <i class=\"fab fa-whatsapp fa-2x\" id=\"whatsapp\"></i>
+         </div>
+    </div>
+  </div>
+</footer>
+"; 
+
+
+?>
---
+++ php/navigation.php
@@ -0,0 +1,24 @@
+<?php
+echo "<nav class=\"navbar navbar-dark bg-dark navbar-expand-lg nav_control\">
+  <a href=\"index.html\" class=\"navbar-brand\"><img src=\"images\N\res\mipmap-xxhdpi\N.png\" alt=\"\" id=\"logo_image\">Notez</a>
+  <button class=\"navbar-toggler\" type=\"button\" data-toggle=\"collapse\" data-target=\"#navbarSupportedContent\" aria-controls=\"navbarSupportedContent\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">
+    <span class=\"navbar-toggler-icon\"></span>
+  </button>
+  <div class=\"collapse navbar-collapse\" id=\"navbarSupportedContent\">
+    <ul class=\"navbar-nav mr-auto\">
+      <li class=\"nav-item active\">
+        <a class=\"nav-link\" href=\"index.html\">Home <span class=\"sr-only\">(current)</span></a>
+      </li>
+      <li class=\"nav-item\">
+        <a class=\"nav-link navbar_links\" href=\"#\">About</a>
+      </li>
+      <li class=\"nav-item\">
+        <a class=\"nav-link navbar_links\" href=\"#\">Contact Us</a>
+      </li>
+      <li class=\"nav-item\">
+        <a class=\"nav-link navbar_links\" href=\"#\">Sign in</a>
+      </li>
+    </ul>
+  </div>
+</nav>";
+ ?>
